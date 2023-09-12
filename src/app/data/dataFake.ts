11864ee9575a89e6e16c862bab2b518c04a408f7
Amanda Moura <mouraamanda168@gmail.com>
2023-09-11 21:47:09 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,27 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Filme novo da Marvel Studios!",
+        "description": "Marvel Studios anuncia novo filme dos Vingadores.",
+        "photoCover": "https://th.bing.com/th/id/R.a39c6fe6a7f9673039d0039ea2db8a13?rik=5mzkJSiavolP7Q&pid=ImgRaw&r=0"
+    },
+    {
+        "id":"2",
+        "title": "Marvel Studios anuncia novo filme do Pantera Negra!",
+        "description": "hahahaha",
+        "photoCover": "https://www.gamerfocus.co/wp-content/uploads/2021/06/Avengers-Pantera-Negra-scaled.jpg"
+    },
+    {
+        "id":"3",
+        "title": "Lançamento de novo filme do Capitão América!",
+        "description": "hahahaha",
+        "photoCover": "https://th.bing.com/th/id/OIP.rjk7ndYP-2dEWvEPL7ZeFQHaEM?w=186&h=105&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    },
+    {
+        "id":"4",
+        "title": "Novo filme do Thor lança ainda esse ano!",
+        "description": "hahahaha",
+        "photoCover": "https://static.papergeek.fr/2020/07/thor.jpg"
+    }
+]
+
